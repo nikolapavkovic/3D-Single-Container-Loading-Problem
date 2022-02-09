0b3d5f1d60cdb154576f4932d3ac0210ea334ca0
nikola pavkovic <nikola.pavkovic@ieee.org>
2022-02-09 17:24:18 +0100

    Fixed indentation and prettified code.

--- src/hr/Base58/RandomDoubleGenerator.java
+++ src/hr/Base58/RandomDoubleGenerator.java
@@ -2,23 +2,30 @@ package hr.Base58;
 
 import java.util.Random;
 
+//Singleton random double number generator to prevent multiple instance creation with the same feed
 public class RandomDoubleGenerator {
 
     private static RandomDoubleGenerator instance;
     private final Random randomGenerator;
 
     private RandomDoubleGenerator() {
+
         randomGenerator = new Random();
+
     }
 
     public static RandomDoubleGenerator getInstance() {
+
         if(instance == null) {
             instance = new RandomDoubleGenerator();
         }
+
         return instance;
     }
 
     public double nextDouble(double upperBound) {
+
         return randomGenerator.nextDouble() * upperBound;
+
     }
 }
