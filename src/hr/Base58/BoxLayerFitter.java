904ffabdeb1c92c9cd9b50dbb11d81e698f97d74
nikola pavkovic <nikola.pavkovic@ieee.org>
2022-02-09 17:21:10 +0100

    Added box rotation optimization.

--- src/hr/Base58/BoxLayerFitter.java
+++ src/hr/Base58/BoxLayerFitter.java
@@ -9,62 +9,88 @@ public class BoxLayerFitter implements IBoxFitter {
         public BoxLayerFitter(double width, double depth, double maxHeight) {
 
             this.maxHeight=maxHeight;
-            root = new Box(0, 0, width, depth);
+
+            root = new Box("root",0, 0, width, depth, 0);
 
         }
 
 
         public void fit(List<Box> boxes) {
+
             Box box;
             Box freeSpaceBox;
+            Box optimalBox;
 
             for (Box value : boxes) {
 
                 freeSpaceBox = value;
 
-                box= this.findNode(this.root, freeSpaceBox.getWidth(), freeSpaceBox.getDepth());
+                optimalBox=BoxRotationManager.getOptimalRotation(this.root,freeSpaceBox.getWidth(), freeSpaceBox.getDepth(), maxHeight);
+                optimalBox.setAsRoot();
+                
+                box= this.findSpace(optimalBox, freeSpaceBox.getWidth(), freeSpaceBox.getDepth());
 
-                if (box != null && freeSpaceBox.getHeight() <= getMaxHeight()) {
+                if (box != null) {
 
-                    freeSpaceBox.placeBox(this.splitNode(box, freeSpaceBox.getWidth(), freeSpaceBox.getDepth()));
+                    freeSpaceBox.placeBox(this.placeAndSplitSearch(box, freeSpaceBox.getWidth(), freeSpaceBox.getDepth()));
 
-                    if (box.isRoot()) {
+                    if (box.isRoot()){
+
                         freeSpaceBox.getPlacedBox().setAsRoot();
+
                     }
+
                 }
             }
         }
 
 
-        public Box findNode(Box root, double width, double depth) {
+        public Box findSpace(Box root, double width, double depth) {
 
             if (!root.isFreeSpaceBox()) {
 
-                Box right = findNode(root.getRight(), width, depth);
-                return (right != null ? right : findNode(root.getDown(), width, depth));
+                Box right = findSpace(root.getRight(), width, depth);
 
-            } else if ((depth <= root.getDepth()) && (width <= root.getWidth())) {
-                return root;
-            } else {
-                return null;
+                if(right!=null){
+
+                    return right;
+
+                }
+
+                else{
+
+                    return findSpace(root.getDown(), width, depth);
+
+                }
+
             }
+            else if ((depth <= root.getDepth()) && (width <= root.getWidth()) && root.getHeight()<maxHeight){
+
+                return root;
+
+            }
+
+
+            else{
+
+                return null;
+
+            }
+
+
         }
 
-        public Box splitNode(Box box, double width, double depth) {
+        public Box placeAndSplitSearch(Box box, double width, double depth) {
 
             box.setAsUsedSpaceBox();
 
-            box.setDown(new Box(box.getOriginX(), box.getOriginZ() + depth, box.getWidth(),
-                    box.getDepth() - depth));
+            box.setDown(new Box(box.getId(),box.getOriginX(), box.getOriginZ() + depth, box.getWidth(),
+                    box.getDepth() - depth, box.getHeight()));
 
-            box.setRight(new Box(box.getOriginX() + width, box.getOriginZ(),
-                    box.getWidth() - width, depth));
+            box.setRight(new Box(box.getId(), box.getOriginX() + width, box.getOriginZ(),
+                    box.getWidth() - width, depth, box.getHeight()));
+
             return box;
         }
-
-
-    public double getMaxHeight() {
-        return maxHeight;
-    }
 
 }
