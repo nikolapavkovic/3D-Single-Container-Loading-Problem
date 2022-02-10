ecc50a186e1e64c8a8e60c5cc94db682c971b132
nikola pavkovic <nikola.pavkovic@ieee.org>
2022-02-10 13:24:43 +0100

    Extracted box generation methods to adequate classes

--- src/hr/Base58/TestSCLP.java
+++ src/hr/Base58/TestSCLP.java
@@ -11,233 +11,88 @@ public class TestSCLP {
     public static final String ANSI_BLUE = "\u001B[34m";
 
 
-    public static  void fitBoxesFromUserInput(){
-
-        List<Box> boxes =new ArrayList<>(0);
-
-        Scanner dataScanner = new Scanner(System.in);
-
-        System.out.println( ANSI_BLUE + "Please enter the container dimensions " + ANSI_RESET);
-
-        System.out.println(ANSI_CYAN + "Please enter the container width: " + ANSI_RESET);
-        double containerWidth= dataScanner.nextDouble();
-
-        System.out.println( ANSI_CYAN + "Please enter the container depth: " + ANSI_RESET);
-        double containerDepth= dataScanner.nextDouble();
-
-        System.out.println( ANSI_CYAN +"Please enter the container height: " + ANSI_RESET);
-        double containerHeight= dataScanner.nextDouble();
-
-        Container container= new Container(containerWidth,containerDepth,containerHeight);
-
-        System.out.println(ANSI_CYAN + "Please enter the number of box types: " + ANSI_RESET);
-
-        int numOfBoxTypes=dataScanner.nextInt();
-
-        double boxWidth, boxDepth, boxHeight;
-
-        int sameBoxTypes;
-
-
-        for(int i = 0; i<numOfBoxTypes; i++){
-
-            System.out.println(ANSI_BLUE + "please enter the dimensions of the box number " + i + ANSI_RESET);
-
-            System.out.println(ANSI_CYAN + "Please enter the box width: " + ANSI_RESET);
-            boxWidth= dataScanner.nextDouble();
-
-            System.out.println(ANSI_CYAN + "Please enter the box depth: " + ANSI_RESET);
-            boxDepth= dataScanner.nextDouble();
-
-            System.out.println(ANSI_CYAN + "Please enter the box height: " + ANSI_RESET);
-            boxHeight= dataScanner.nextDouble();
-
-            System.out.println("How many boxes with these dimensions do you have? ");
-
-            sameBoxTypes = dataScanner.nextInt();
-
-            for(int j = 0; j< sameBoxTypes; j++){
-
-            boxes.add(new Box("Box type "+ i,0,0 ,boxWidth, boxDepth, boxHeight ));
-
-            }
-
-        }
-
-        BoxLayerStacker boxLayerStacker= new BoxLayerStacker(boxes);
-
-        List<List<Box>> layers =boxLayerStacker.stackLayers(container);
-
-        int layerIndex = 0;
-
-        for (List<Box> layer: layers) {
-
-            System.out.println(ANSI_GREEN + "-----------------------------layer "+ ++layerIndex + "-----------------------------\n" + ANSI_RESET);
-
-            for (Box box: layer) {
-
-                System.out.println(box.toString()+"\n");
-
-            }
-
-        }
-
-        System.out.println(ANSI_GREEN + "Box fitting complete, the total amount of placed boxes is: " + boxLayerStacker.getNumberOfPlacedBoxes() + ANSI_RESET);
-
-        System.out.println();
-
-    }
-
-
-    public static  void fitRandomBoxes(){
-
-        double maxWidth, maxDepth, maxHeight;
-
-        Scanner dataScanner = new Scanner(System.in);
-
-        System.out.println( ANSI_BLUE + "Please enter the container dimensions " + ANSI_RESET);
-
-        System.out.println(ANSI_CYAN + "Please enter the container width: " + ANSI_RESET);
-        double containerWidth= dataScanner.nextDouble();
-
-        System.out.println( ANSI_CYAN + "Please enter the container depth: " + ANSI_RESET);
-        double containerDepth= dataScanner.nextDouble();
-
-        System.out.println( ANSI_CYAN +"Please enter the container height: " + ANSI_RESET);
-        double containerHeight= dataScanner.nextDouble();
-
-        Container container= new Container(containerWidth,containerDepth,containerHeight);
-
-        System.out.println(ANSI_BLUE + "please enter the maximum dimensions of a randomly generated box"  + ANSI_RESET);
-
-        System.out.println(ANSI_CYAN + "Please enter the maximum box width: " + ANSI_RESET);
-        maxWidth= dataScanner.nextDouble();
-
-        System.out.println(ANSI_CYAN + "Please enter the maximum box depth: " + ANSI_RESET);
-        maxDepth= dataScanner.nextDouble();
-
-        System.out.println(ANSI_CYAN + "Please enter the maximum box height: " + ANSI_RESET);
-        maxHeight= dataScanner.nextDouble();
-
-        System.out.println(ANSI_CYAN + "Please enter the number of boxes to generate: " + ANSI_RESET);
-        int numberOfBoxes= dataScanner.nextInt();
-
-        List<Box> boxes =RandomBoxGenerator.generateRandomBoxes(numberOfBoxes, maxWidth, maxDepth, maxHeight, RandomDoubleGenerator.getInstance());
-
-        System.out.println("Generated boxes:\n");
-        for (Box box:boxes) {
-            System.out.println(box.toString());
-        }
-
-
-        BoxLayerStacker boxLayerStacker= new BoxLayerStacker(boxes);
-
-        List<List<Box>> layers =boxLayerStacker.stackLayers(container);
-
-
-        int layerIndex = 0;
-
-        for (List<Box> layer: layers) {
-
-            System.out.println(ANSI_GREEN + "-----------------------------layer "+ ++layerIndex + "-----------------------------\n" + ANSI_RESET);
-
-            for (Box box: layer) {
-
-                System.out.println(box.toString()+"\n");
-
-            }
-
-        }
-
-        System.out.println(ANSI_GREEN + "Box fitting complete, the total amount of placed boxes is: " + boxLayerStacker.getNumberOfPlacedBoxes() + ANSI_RESET);
-
-        System.out.println();
-
-
-    }
-
-    public static  void fitSameSizedBoxes(){
-
-    }
-
-
-
-
     public static void main(String[] args) {
 
 
-        fitRandomBoxes();
-        //fitBoxesFromUserInput();
-
-
-
-        /*Container container= new Container(600.18,600,1000);
-
-        List<Box> boxes =new ArrayList<>(0);
-        //RandomBoxGenerator.generateRandomBoxes(51,100,100,500,
-        //  RandomDoubleGenerator.getInstance());
-
-
-        boxes.add(new Box("Figure1", 0,0,600, 600, 500));
-        //boxes.add(new Box("Figure2", 0,0,590, 400, 400));
-        boxes.add(new Box("Figure3", 0,0, 100, 100, 500));
-
-
-
-
-
-
- /*
         Scanner dataScanner = new Scanner(System.in);
-        System.out.println("Please enter the number of box types: ");
-        int numOfBoxTypes=dataScanner.nextInt();
 
-        double X,Y,Z;
+        System.out.println( ANSI_BLUE + "Please enter the container dimensions " + ANSI_RESET);
 
-        int sameBoxTypes;
+        System.out.println(ANSI_CYAN + "Please enter the container width: " + ANSI_RESET);
+        double containerWidth = dataScanner.nextDouble();
 
-        int boxID;
+        System.out.println( ANSI_CYAN + "Please enter the container depth: " + ANSI_RESET);
+        double containerDepth = dataScanner.nextDouble();
 
-        for(int i = 0; i<numOfBoxTypes; i++){
-            boxID=i+1;
-            System.out.println("please enter the dimensions of the box number " + boxID);
-            X= dataScanner.nextDouble();
-            Y= dataScanner.nextDouble();
-            Z= dataScanner.nextDouble();
-            System.out.println("How many boxes with these dimensions do you have? ");
-            int numOfBoxType = dataScanner.nextInt();
-            boxes.add(new Box("Box "+ i ,X, Y, Z ));
+        System.out.println( ANSI_CYAN +"Please enter the container height: " + ANSI_RESET);
+        double containerHeight = dataScanner.nextDouble();
+
+        Container container = new Container(containerWidth, containerDepth, containerHeight);
+
+
+
+        System.out.println( ANSI_BLUE +"Please enter the type of box generation.\n0 => random box generation\n1 =>" +
+                                       " box generation from user input\n" +
+                                       "2 => generate certain number of same-sized boxes" + ANSI_RESET);
+
+
+        int boxGenerationIndex;
+
+        do{
+            boxGenerationIndex= dataScanner.nextInt();
+
+            if(!(boxGenerationIndex>=0 && boxGenerationIndex<=2)){
+
+                System.out.println("Please enter a valid box generation type (0 - 2) ");
+
+            }
+        }
+        while(!(boxGenerationIndex >= 0 && boxGenerationIndex <= 2));
+
+        IBoxGenerator boxGenerator;
+
+        if(boxGenerationIndex == 0){
+
+            boxGenerator = new RandomBoxGenerator(RandomDoubleGenerator.getInstance());
+
+        }
+        else if(boxGenerationIndex == 1){
+
+            boxGenerator = new UserInputBoxGenerator();
+
+        }
+        else{
+
+            boxGenerator= new SameSizedBoxGenerator();
 
         }
 
+        List<Box> boxes= boxGenerator.getBoxes();
 
 
 
 
-     //   boxes.sort(new WidthComparator());
-
-       // List<Box> boxes1 = new ArrayList<>(boxes);
-
-       // BoxLayerStacker boxLayerStacker= new BoxLayerStacker(boxes1);
-
+        BoxLayerStacker boxLayerStacker= new BoxLayerStacker(boxes);
 
         List<List<Box>> layers =boxLayerStacker.stackLayers(container);
 
-        int layerIndex=0;
+        int layerIndex = 0;
+
         for (List<Box> layer: layers) {
 
             System.out.println(ANSI_GREEN + "-----------------------------layer "+ ++layerIndex + "-----------------------------\n" + ANSI_RESET);
 
             for (Box box: layer) {
-                System.out.println(box.toString()+"\n");
+
+                System.out.println(box.toString()+ box.getPlacementCoordinateDetails() + "\n");
+
             }
+
         }
-        System.out.println(ANSI_GREEN+"Box fitting complete, the total amount of placed boxes is: " + boxLayerStacker.getNumberOfPlacedBoxes()+ANSI_RESET);
+
+        System.out.println(ANSI_GREEN + "Box fitting complete, the total amount of placed boxes is: " + boxLayerStacker.getNumberOfPlacedBoxes() + ANSI_RESET);
+
         System.out.println();
-
-
-
-*/
     }
 
 }
